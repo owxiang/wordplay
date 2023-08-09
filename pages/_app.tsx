6d39c5cf07d216421050887b5fb6d6a12f4963df
owxiang <ong_wei_xiang@defence.gov.sg>
2023-08-09 12:45:14 +0800

    chore: imported AppProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
 import '../styles/globals.css';
 import { EmailProvider } from '../contexts/EmailContext';
+import { AppProps } from 'next/app'; // Importing the type
 
-function MyApp({ Component, pageProps }) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [email, setEmail] = useState('');
 
   return (
-    
     <EmailProvider>
-    <Component {...pageProps} />
-  </EmailProvider>
+      <Component {...pageProps} />
+    </EmailProvider>
   );
 }
 
 export default MyApp;
-
-
-
-
